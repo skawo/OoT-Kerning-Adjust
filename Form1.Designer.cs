3726e677d93bf87d2492a7edff31855ad64f3d10
skawo <43761362+skawo@users.noreply.github.com>
2025-07-30 19:45:09 +0200

    all the stuff

---
+++ Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿
+namespace KerningAdjuster
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Char = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Kerning = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(358, 366);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(430, 238);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(358, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(430, 348);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Char,
+            this.Kerning});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 59);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(340, 545);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellValueChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(163, 41);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(181, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(171, 41);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Char
+            // 
+            this.Char.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Char.HeaderText = "Chara";
+            this.Char.Name = "Char";
+            this.Char.ReadOnly = true;
+            this.Char.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // Kerning
+            // 
+            this.Kerning.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Kerning.HeaderText = "Kerning";
+            this.Kerning.Name = "Kerning";
+            this.Kerning.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 616);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "OoT Kerning Adjuster";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Char;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Kerning;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace KerningAdjuster
+{
+    public partial class Form1 : Form
+    {
+        List<float> fontWidths;
+        byte[] font;
+        List<string> codepoint;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog OF = new OpenFileDialog();
+            OF.Title = "Select your font_widths file.";
+            OF.Filter = "Width table (*.width_table)|*.width_table|All files (*.*)|*.*";
+
+            try
+            {
+                if (OF.ShowDialog() == DialogResult.OK)
+                {
+                    byte[] widths = File.ReadAllBytes(OF.FileName);
+                    fontWidths = new List<float>();
+
+                    for (int i = 0; i < widths.Length; i += 4)
+                    {
+                        byte[] width = widths.Skip(i).Take(4).Reverse().ToArray();
+                        fontWidths.Add(BitConverter.ToSingle(width, 0));
+                    }
+                }
+                else
+                    return;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: ", ex.Message);
+                return;
+            }
+
+            OF.Title = "Select your font_static file.";
+            OF.Filter = "Font (*.font_static)|*.font_static|All files (*.*)|*.*";
+
+            try
+            {
+                if (OF.ShowDialog() == DialogResult.OK)
+                {
+                    font = File.ReadAllBytes(OF.FileName);
+
+                }
+                else
+                    return;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: ", ex.Message);
+                return;
+            }
+
+            OF.Title = "Select your codepoint.";
+            OF.Filter = "Codepoint (*.txt)|*.txt|All files (*.*)|*.*";
+
+            try
+            {
+                if (OF.ShowDialog() == DialogResult.OK)
+                {
+                    codepoint = File.ReadAllLines(OF.FileName).ToList();
+                }
+                else
+                    return;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: ", ex.Message);
+                return;
+            }
+
+            FillData();
+
+
+        }
+
+        private void FillData()
+        {
+            dataGridView1.Rows.Clear();
+
+            for (int i = 1; i < codepoint.Count; i++)
+            {
+
+                if (fontWidths.Count > i - 1)
+                    dataGridView1.Rows.Add(new object[] { codepoint[i], fontWidths[i - 1] });
+            }
+
+            textBox1.Text = codepoint[0].Replace("\\n", Environment.NewLine);
+
+
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            NewPreview(textBox1.Text);
+        }
+
+        public void NewPreview(string text)
+        {
+            List<byte> codes = new List<byte>();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == '\n')
+                    codes.Add((byte)ZeldaMessage.Data.MsgControlCode.LINE_BREAK);
+                else if (text[i] == '\r')
+                    continue;
+                else if (text[i] == ' ')
+                    codes.Add(0x20);
+                else
+                {
+                    int idx = codepoint.FindIndex(x => x == text[i].ToString().Trim());
+
+                    if (idx != -1)
+                        codes.Add((byte)(idx + 0x1F));
+                }
+            }
+
+            codes.Add((byte)ZeldaMessage.Data.MsgControlCode.PERSISTENT);
+
+            ZeldaMessage.MessagePreview pr = new ZeldaMessage.MessagePreview(ZeldaMessage.Data.BoxType.None_Black, codes.ToArray(), fontWidths.ToArray(), font, true);
+            Bitmap b = pr.GetPreview(0, true, 1.5f);
+
+            if (b != null)
+                pictureBox1.Image = b;
+
+        }
+
+        private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            List<float> newWidths = new List<float>();
+
+            int curIndex = 0;
+
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (row.Cells.Count != 2)
+                    return;
+
+                float f;
+                string val;
+
+                try
+                {
+                    if (row.Cells[1].Value == null)
+                        continue;
+
+                    val = row.Cells[1].Value.ToString();
+                    f = (float)Convert.ToDecimal(val);
+
+                }
+                catch (Exception ex)
+                {
+                    row.Cells[1].Value = fontWidths[curIndex].ToString();
+                    f = fontWidths[curIndex];
+                }
+
+                newWidths.Add(f);
+                curIndex++;
+            }
+
+            fontWidths = newWidths;
+            NewPreview(textBox1.Text);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog SF = new SaveFileDialog();
+            SF.FileName = "font.font_widths";
+
+            if (SF.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    List<byte> allBytes = new List<byte>();
+
+                    foreach (float width in fontWidths)
+                    {
+                        byte[] floatBytes = BitConverter.GetBytes(width);
+                        byte[] reversedBytes = floatBytes.Reverse().ToArray();
+                        allBytes.AddRange(reversedBytes);
+                    }
+
+                    File.WriteAllBytes(SF.FileName, allBytes.ToArray());
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: ", ex.Message);
+                    return;
+                }
+            }
+        }
+    }
+}
